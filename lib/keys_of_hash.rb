871863bc38cf633ba6a03bcff0a7e29e3687d020
layla dantas <laylatdantas@gmail.com>
2019-06-02 23:25:29 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,16 +3,8 @@ class Hash
   def keys_of(arguments)
     puts self
     array = []
-    if (arguments== 'Panama')
-      array.push("red-footed tortoise") 
-    elsif (arguments == 'Madagascar')
-      array.push("aye-aye")
-      array.push("tomato frog")
-    elsif (arguments == 'Australia')
-      array.push("sugar glider")
-      array.push("kangaroo")
-      array.push("koala")
-      end
-    array  
+    self.each do |k,v|
+      puts "#{k} = #{v}"
+    end
   end
 end
