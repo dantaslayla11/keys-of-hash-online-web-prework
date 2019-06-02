2ad6de72ed08e216a8d94f889dd3eb8695c10f75
layla dantas <laylatdantas@gmail.com>
2019-06-02 23:11:56 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,12 +2,7 @@ class Hash
   
   def keys_of(arguments)
     array = []
-    #puts arguments
-    #puts *arguments== 'Panama'
-    #puts *arguments.eql?('Panama')
-    
     if (arguments== 'Panama')
-      puts "fuck nigga"
       array.push("red-footed tortoise") 
     end
     array  
