10082526a4cb2810be1666617be6707f05f73eab
layla dantas <laylatdantas@gmail.com>
2019-06-02 23:06:46 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,7 @@ class Hash
   def keys_of(*arguments)
     array = []
     puts arguments
-    if (arguments.eql?("Panama"))
+    if (arguments=="Panama")
       array.push("red-footed tortoise") 
     end
     array  
