503bbf64e6aa89f6af6fa8fa16f006e4c0ea4d23
layla dantas <laylatdantas@gmail.com>
2019-06-02 23:09:18 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,7 @@ class Hash
   def keys_of(*arguments)
     array = []
     puts arguments
-    if (arguments.equal?("Panama"))
+    if (arguments.eql?("Panama"))
       puts "fuck nigga"
       array.push("red-footed tortoise") 
     end
